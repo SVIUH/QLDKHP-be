5e9df9d3656683d2186e422fdbd15cadffa4b440
Nhut Hao <c520a1@gmail.com>
2025-04-10 04:11:20 +0700

    fix schema, model class, grade

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SubjectModule } from './subject/subject.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [SubjectModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
